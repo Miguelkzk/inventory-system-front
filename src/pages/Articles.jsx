2a3970fc7b53108bf807389ce7b67b95eab1c52f
Miguelkzk <141929749+Miguelkzk@users.noreply.github.com>
2024-05-24 16:52:23 -0300

    dark mode and articles table

--- src/pages/Articles.jsx
+++ src/pages/Articles.jsx
@@ -1,6 +1,26 @@
+import { Button, Table } from "react-bootstrap";
+
 function ArticlesTable() {
   return (
-    <h1>Articles</h1>
+    <>
+      <div className="container mt-3">
+        <Button>New article</Button>
+
+        <Table hover style={{ fontSize: '18px', marginTop: '1rem' }}>
+          <thead style={{ textAlign: 'center' }} >
+            <tr>
+              <th>Code</th>
+              <th>Name</th>
+              <th>Description</th>
+              <th>Current stock</th>
+            </tr>
+          </thead>
+          <tbody>
+          </tbody>
+        </Table >
+      </div>
+    </>
+
   )
 }
 export default ArticlesTable;
